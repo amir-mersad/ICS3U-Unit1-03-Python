7a9c812fc774372de38ea25f91facbc86967b561
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-09-12 18:33:21 +0000

    program added

---
+++ hello-world-proper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Amir Mersad
+# Created on September 2019
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World I'm here.")
+
+
+if __name__ == "__main__":
+    main()
